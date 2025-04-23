f6063b33f358cb6a368980e64d268047aef33de6
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2025-04-23 15:47:15 +0000

    install all deependencies

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# CoverAi
+# coverai
+
+Add these to your .env file
+
+JWTSECRET
+CONNECTIONSTRING (MONGODB)
+
+mongodb+.jni44ig.mongodb.net/{database}?retryWrites=true&w=majority&appName=Cluster0
---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import RegisterForm from "@/components/RegisterForm";
+
+export default function Page() {
+  return (
+    <div className="min-h-[85vh] container mx-auto grid grid-cols-1 md:grid-cols-2 px-3">
+      <div className="flex items-center">
+        <h1 className="text-amber-400 text-3xl md:text-4xl font-light tracking-wide">
+          CoverAI &mdash; Create Engaging Thumbnails <br />
+          using GenAI
+        </h1>
+      </div>
+
+      {/* Register Form */}
+      <div className="flex items-start md:items-center">
+        <RegisterForm />
+      </div>
+    </div>
+  );
+}
